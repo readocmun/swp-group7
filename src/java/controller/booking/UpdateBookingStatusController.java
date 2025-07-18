09350bcf035aa15a35e5bf9bc464563d62db036c
nhaq0505 <anhquan050503@gmail.com>
2025-07-18 07:08:08 +0700

    FixBooking

--- src/java/controller/booking/UpdateBookingStatusController.java
+++ src/java/controller/booking/UpdateBookingStatusController.java
@@ -84,7 +84,14 @@ public class UpdateBookingStatusController extends HttpServlet {
         processRequest(request, response);
     }
 
-   
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
